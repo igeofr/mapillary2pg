d074e7380f5809602d3be4c36a983930d0f114da
Flø <florian.boret@laposte.net>
2022-05-06 16:15:38 +0200

    Update mapillary_vt2pg.sh

--- mapillary_vt2pg.sh
+++ mapillary_vt2pg.sh
@@ -5,6 +5,35 @@
 # CC BY-SA 4.0 : https://creativecommons.org/licenses/by-sa/4.0/deed.fr
 #-------------------------------------------------------------------------------
 
+# RECUPERATION DU TYPE DE DATA A INTEGRER (image ou point)
+if [ "$#" -ge 1 ]; then
+  if [ "$1" = "image" ]  || [ "$1" = "point" ];
+  then
+    TYPE=$1
+    echo $TYPE
+  else
+  IFS= read -p "Type : " S_TYPE
+    if [ "$S_TYPE" = "image" ]  || [ "$S_TYPE" = "point" ];
+    then
+      export TYPE=$S_TYPE
+      echo $TYPE
+    else
+      echo "Erreur de paramètre"
+      exit 0
+    fi
+  fi
+else
+  IFS= read -p "Type : " S_TYPE
+  if [ "$S_TYPE" = "image" ]  || [ "$S_TYPE" = "point" ];
+  then
+    export TYPE=$S_TYPE
+    echo $TYPE
+  else
+    echo "Erreur de paramètre"
+    exit 0
+  fi
+fi
+
 # VARIABLES DATES
 export DATE_YM=$(date "+%Y%m")
 export DATE_YMD=$(date "+%Y%m%d")
@@ -31,40 +60,54 @@ lat2ytile() {
  ytile=`echo "${lat} ${zoom}" | awk -v PI=3.14159265358979323846 '{
    tan_x=sin($1 * PI / 180.0)/cos($1 * PI / 180.0);
    ytile = (1 - log(tan_x + 1/cos($1 * PI/ 180))/PI)/2 * 2.0^$2;
+   ytile+=ytile<0?-0.5:0.5;
    printf("%d", ytile ) }'`;
  echo -n "${ytile}";
 }
-XMIN=$(long2xtile $V_XMIN $V_ZOOM)
-XMAX=$(long2xtile $V_XMAX $V_ZOOM)
-YMIN=$(lat2ytile $V_YMIN $V_ZOOM)
-YMAX=$(lat2ytile $V_YMAX $V_ZOOM)
+
+XMIN=$(long2xtile $(echo $V_XMIN | sed -e 's/\./,/g') $V_ZOOM)
+XMAX=$(long2xtile $(echo $V_XMAX | sed -e 's/\./,/g') $V_ZOOM)
+YMIN=$(lat2ytile $(echo $V_YMIN | sed -e 's/\./,/g') $V_ZOOM)
+YMAX=$(lat2ytile $(echo $V_YMAX | sed -e 's/\./,/g') $V_ZOOM)
 echo $XMIN $YMIN $XMAX $YMAX
 
 #-------------------------------------------------------------------------------
 # IMAGE ET SEQUENCE
 echo 'Debut du traitement des données de Mapillary'
 
-file=$REPER'/'$DATE_YMD'_MAPILLARY_VT_IMAGE_SEQUENCE.gpkg'
-rm $REPER'/'$DATE_YMD'_MAPILLARY_VT_IMAGE_SEQUENCE.'*
-file_pt=$REPER'/'$DATE_YMD'_MAPILLARY_VT_POINT_DETETE.gpkg'
-rm $REPER'/'$DATE_YMD'_MAPILLARY_VT_POINT_DETECTE.'*
+if  [ "$TYPE" = "image" ]; then
+  L_TYPE="image_sequence"
+  VAR_URL="mly1_public"
+  LAYER="sequence image"
+elif [ "$TYPE" = "point" ]
+then
+  L_TYPE="point_detecte"
+  VAR_URL="mly_map_feature_point"
+  LAYER="point"
+fi
+
+file=$REPER'/'$DATE_YMD'_MAPILLARY_VT_'$L_TYPE'.gpkg'
+rm $REPER'/'$DATE_YMD'_MAPILLARY_VT_'$L_TYPE'.'*
+
+rm -r -d $REPER'/tuiles/tuiles_'$L_TYPE'/'${DATE_YMD}
 
 Z=$V_ZOOM
 for X in $(seq $XMIN $XMAX);do
-   for Y in $(seq $YMIN $YMAX);do
+   for Y in $(seq $YMAX $YMIN);do
 
-      MVTFILE=${Z}'_'${X}'_'${Y}'.pbf'
+      PBF_FILE=${Z}'_'${X}'_'${Y}'.pbf'
 
       #-------------------------------------------------------------------------------
-      URL="https://tiles.mapillary.com/maps/vtp/mly1_public/2/$Z/$X/$Y?access_token=$TOKEN"
+      URL="https://tiles.mapillary.com/maps/vtp/$VAR_URL/2/$Z/$X/$Y?access_token=$TOKEN"
+      #echo "https://tiles.mapillary.com/maps/vtp/$VAR_URL/2/$Z/$X/$Y?access_token=$TOKEN"
 
-      mkdir $REPER'/tuiles/tuiles_image_sequence/'${DATE_YMD}
-      mkdir $REPER'/tuiles/tuiles_image_sequence/'${DATE_YMD}'/'${Z}
-      mkdir $REPER'/tuiles/tuiles_image_sequence/'${DATE_YMD}'/'${Z}'/'${X}
-      mkdir $REPER'/tuiles/tuiles_image_sequence/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}
+      mkdir $REPER'/tuiles/tuiles_'$L_TYPE'/'${DATE_YMD}
+      mkdir $REPER'/tuiles/tuiles_'$L_TYPE'/'${DATE_YMD}'/'${Z}
+      mkdir $REPER'/tuiles/tuiles_'$L_TYPE'/'${DATE_YMD}'/'${Z}'/'${X}
+      mkdir $REPER'/tuiles/tuiles_'$L_TYPE'/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}
 
       # TELECHARGEMENT DES TUILES
-      curl -w "%{http_code}" $URL --max-time 120 --connect-timeout 60 -o $REPER'/tuiles/tuiles_image_sequence/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}'/'$MVTFILE
+      curl -w "%{http_code}" $URL --max-time 120 --connect-timeout 60 -o $REPER'/tuiles/tuiles_'$L_TYPE'/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}'/'$PBF_FILE
 
       # FUSION EN GPKG
       ogr2ogr \
@@ -74,30 +117,7 @@ for X in $(seq $XMIN $XMAX);do
       --debug ON \
       -lco SPATIAL_INDEX=YES \
       $file \
-      $REPER'/tuiles/tuiles_image_sequence/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}'/'$MVTFILE sequence image \
-      -nlt PROMOTE_TO_MULTI \
-      -oo x=${X} -oo y=${Y} -oo z=${Z}
-
-      #-------------------------------------------------------------------------------
-      URL_PT="https://tiles.mapillary.com/maps/vtp/mly_map_feature_point/2/$Z/$X/$Y?access_token=$TOKEN"
-
-      mkdir $REPER'/tuiles/tuiles_point_detecte/'${DATE_YMD}
-      mkdir $REPER'/tuiles/tuiles_point_detecte/'${DATE_YMD}'/'${Z}
-      mkdir $REPER'/tuiles/tuiles_point_detecte/'${DATE_YMD}'/'${Z}'/'${X}
-      mkdir $REPER'/tuiles/tuiles_point_detecte/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}
-
-      # TELECHARGEMENT DES TUILES
-      curl -w "%{http_code}" $URL_PT --max-time 120 --connect-timeout 60 -o $REPER'/tuiles/tuiles_point_detecte/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}'/'$MVTFILE
-
-      # FUSION EN GPKG
-      ogr2ogr \
-      -progress \
-      -f 'GPKG' \
-      -update -append \
-      --debug ON \
-      -lco SPATIAL_INDEX=YES \
-      $file_pt \
-      $REPER'/tuiles/tuiles_point_detecte/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}'/'$MVTFILE point \
+      $REPER'/tuiles/tuiles_'$L_TYPE'/'${DATE_YMD}'/'${Z}'/'${X}'/'${Y}'/'$PBF_FILE $LAYER \
       -nlt PROMOTE_TO_MULTI \
       -oo x=${X} -oo y=${Y} -oo z=${Z}
 
@@ -107,43 +127,45 @@ done
 echo 'Import dans PG'
 
 # IMPORT PG
-ogr2ogr \
-    -append \
-    -f "PostgreSQL" PG:"host='$C_HOST' user='$C_USER' dbname='$C_DBNAME' password='$C_PASSWORD' schemas='$C_SCHEMA'" \
-    -nln 'mapillary_vt_sequence' \
-    -s_srs 'EPSG:3857' \
-    -t_srs 'EPSG:2154' \
-    $file 'sequence' \
-    -where "captured_at>$DATE_DEBUT_T" \
-    -dialect SQLITE \
-    --config OGR_TRUNCATE YES \
-    --debug ON \
-    --config CPL_LOG './'$REPER_LOGS'/'$DATE_YMD'_mapillary_vt_sequence.log'
-
-ogr2ogr \
-    -append \
-    -f "PostgreSQL" PG:"host='$C_HOST' user='$C_USER' dbname='$C_DBNAME' password='$C_PASSWORD' schemas='$C_SCHEMA'" \
-    -nln 'mapillary_vt_image' \
-    -s_srs 'EPSG:3857' \
-    -t_srs 'EPSG:2154' \
-    $file 'image' \
-    -where "captured_at>$DATE_DEBUT_T" \
-    -dialect SQLITE \
-    --config OGR_TRUNCATE YES \
-    --debug ON \
-    --config CPL_LOG './'$REPER_LOGS'/'$DATE_YMD'_mapillary_vt_image.log'
-
-ogr2ogr \
-    -append \
-    -f "PostgreSQL" PG:"host='$C_HOST' user='$C_USER' dbname='$C_DBNAME' password='$C_PASSWORD' schemas='$C_SCHEMA'" \
-    -nln 'mapillary_vt_point' \
-    -s_srs 'EPSG:3857' \
-    -t_srs 'EPSG:2154' \
-    $file_pt 'point' \
-    -where "last_seen_at>$DATE_DEBUT_T" \
-    -dialect SQLITE \
-    --config OGR_TRUNCATE YES \
-    --debug ON \
-    --config CPL_LOG './'$REPER_LOGS'/'$DATE_YMD'_mapillary_vt_point.log'
+if  [ "$TYPE" = "image" ]; then
+    ogr2ogr \
+        -append \
+        -f "PostgreSQL" PG:"host='$C_HOST' user='$C_USER' dbname='$C_DBNAME' password='$C_PASSWORD' schemas='$C_SCHEMA'" \
+        -nln 'mapillary_vt_sequence' \
+        -s_srs 'EPSG:3857' \
+        -t_srs 'EPSG:2154' \
+        $file 'sequence' \
+        -where "captured_at>$DATE_DEBUT_T" \
+        -dialect SQLITE \
+        --config OGR_TRUNCATE YES \
+        --debug ON \
+        --config CPL_LOG './'$REPER_LOGS'/'$DATE_YMD'_mapillary_vt_sequence.log'
+    ogr2ogr \
+        -append \
+        -f "PostgreSQL" PG:"host='$C_HOST' user='$C_USER' dbname='$C_DBNAME' password='$C_PASSWORD' schemas='$C_SCHEMA'" \
+        -nln 'mapillary_vt_image' \
+        -s_srs 'EPSG:3857' \
+        -t_srs 'EPSG:2154' \
+        $file 'image' \
+        -where "captured_at>$DATE_DEBUT_T" \
+        -dialect SQLITE \
+        --config OGR_TRUNCATE YES \
+        --debug ON \
+        --config CPL_LOG './'$REPER_LOGS'/'$DATE_YMD'_mapillary_vt_image.log'
+elif [ "$TYPE" = "point" ]
+then
+    ogr2ogr \
+        -append \
+        -f "PostgreSQL" PG:"host='$C_HOST' user='$C_USER' dbname='$C_DBNAME' password='$C_PASSWORD' schemas='$C_SCHEMA'" \
+        -nln 'mapillary_vt_point' \
+        -s_srs 'EPSG:3857' \
+        -t_srs 'EPSG:2154' \
+        $file 'point' \
+        -where "last_seen_at>$DATE_DEBUT_T" \
+        -dialect SQLITE \
+        --config OGR_TRUNCATE YES \
+        --debug ON \
+        --config CPL_LOG './'$REPER_LOGS'/'$DATE_YMD'_mapillary_vt_point.log'
+fi
 
 echo 'Fin du traitement des données de Mapillary'
